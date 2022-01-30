c7ecde718d2aff74879d862a6cbda460445276fa
Elena <ferfox@yandex.ru>
2022-01-30 17:35:29 +0300

    first commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,13 @@
+const timer = document.getElementById('timer');
+let timerCount = +timer.textContent
+
+const timerInterval = setInterval(() => {
+    if (timerCount === 0) {
+        alert('Вы победили в конкурсе!');
+    } else {
+        timerCount -= 1;
+        timer.textContent = timerCount;
+    }
+}, 1000);
+
+console.log(timerCount)
